6600ac73733bdaf4bd5e49ccfc1e0bf95215b23d
juliethaguti <juliethaguti@gmail.com>
2017-02-02 22:12:43 -0300

    Segundo commit
    Ejercicios hechos en clase del 4 al 6

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,7 @@
 */
 function Mostar()
 {
-	
+	var Nombre= prompt("Ingrese su nombre");
+	document.getElementById('elNombre').value=Nombre;
 }
 
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,11 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+var nombre
+var Edad
+
+nombre=document.getElementById('elNombre').value;
+Edad=document.getElementById('laEdad').value;
+alert("Usted se llama "+nombre+" y tiene "+Edad+" años");
 }
 
