e9939623cbd8f79bef9e53fd7eb73b0ae0677fbc
juliethaguti <juliethaguti@gmail.com>
2017-02-10 21:13:08 -0300

    Sexto commit
    Ejercicios hechos en clase

---
+++ jsInstruccionSwitch(3).js
@@ -0,0 +1,28 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+	switch(mesDelAño){
+		case "Enero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+		case "Julio":
+		case "Agosto":
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+		alert("Este mes tiene 30 o más días");
+		break;
+		case "Febrero":
+		alert("Este mes no tiene más de 29 días");
+		break;
+	} 
+	
+
+
+}//FIN DE LA FUNCIÓN
