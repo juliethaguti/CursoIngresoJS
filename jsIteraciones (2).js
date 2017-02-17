d71e5240f48c9dfbc8bcb9a628cce14d5f62f2f3
juliethaguti <juliethaguti@gmail.com>
2017-02-17 16:43:21 -0300

    Decimo commit
    Hecho en casa

---
+++ jsIteraciones (2).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+	var numero=10;
+	var contador=0;
+
+	//alert('iteración while');
+	while(contador<10){
+		
+		contador++;
+		alert(numero);
+		numero=numero-1;
+
+	}
+
+}//FIN DE LA FUNCIÓN
